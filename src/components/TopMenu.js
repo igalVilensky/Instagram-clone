55a93929b9a0c67941dc2d12ccc6d243837fb5c4
igalv <vilenskyigal@gmail.com>
2021-06-02 12:27:27 +0200

    Top menu component

---
+++ src/components/TopMenu.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function TopMenu() {
+  return (
+    <div className="app__header">
+      <div className="app__headerImage">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="logo"
+        ></img>
+        HEllo
+      </div>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default TopMenu;
