0e7fb46ce3d2074bba356d08741c0ea08d4b6899
igalv <vilenskyigal@gmail.com>
2021-06-10 12:18:31 +0200

    hover fix

--- src/components/PostsImages.js
+++ src/components/PostsImages.js
@@ -5,135 +5,153 @@ function PostsImages() {
   return (
     <div>
       <div className="posts__items__wrap">
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1488654715439-fbf461f0eb8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3F1YXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="https://images-na.ssl-images-amazon.com/images/I/A1yGF8BpRoL.jpg"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1504600770771-fb03a6961d33?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3F1YXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="https://static-cse.canva.com/blob/130581/07-gross_IG_hubs_1_perspective.png"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1495615080073-6b89c9839ce0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNxdWFyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="https://media.timeout.com/images/103880032/image.jpg"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
       </div>
       <div className="posts__items__wrap">
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1581499230598-7a90d541c73a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fHNxdWFyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="https://blog.hubspot.com/hs-fs/hubfs/no-your-city-instagram-radio-city.png?width=278&name=no-your-city-instagram-radio-city.png"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1592660716763-09efba6db4e3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHNxdWFyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2017/09/abcccccccccccccccc.jpg"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
-        <div className="post__image__wrap">
+        <div className="main-wrap">
+          <img
+            src="https://i.guim.co.uk/img/media/5c584d158992966c11c5f401e5a52e71014954c5/0_289_3992_2396/master/3992.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=7ec3e1fb9911d1f87ba3455a5877ea41"
+            alt="post"
+          ></img>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="posts__items__wrap">
+        <div className="main-wrap">
           <img
             src="https://images.unsplash.com/photo-1612450632008-22c2a5437dc1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHNxdWFyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="posts__items__wrap">
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1488654715439-fbf461f0eb8d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3F1YXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="https://media1.popsugar-assets.com/files/thumbor/VEGiJmgc7O2_cRn0RPX87MuyE-I/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/03/14/850/n/41420188/e198c7db5c8aaaa3a0f375.84833098_/i/Best-Instagram-Spots-New-York-City.jpg"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
-        <div className="post__image__wrap">
+        <div className="main-wrap">
           <img
-            src="https://images.unsplash.com/photo-1504600770771-fb03a6961d33?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c3F1YXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            src="http://annetravelfoodie.com/wp-content/uploads/2018/11/elbhilharmonie-hamburg-instagram-1024x1024.png"
             alt="post"
           ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart" />
-          <span className="post__item__likes__counter">
-            <strong>538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
-        </div>
-        <div className="post__image__wrap">
-          <img
-            src="https://images.unsplash.com/photo-1495615080073-6b89c9839ce0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNxdWFyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-            alt="post"
-          ></img>
-          <FontAwesomeIcon icon={faHeart} className="heart hover" />
-          <span className="post__item__likes__counter">
-            <strong className="hover">538k</strong>
-          </span>
-          <FontAwesomeIcon icon={faComment} className="comment" />
-          <span className="post__item__comments__counter">
-            <strong>2,572</strong>
-          </span>
+          <div className="second-div">
+            <FontAwesomeIcon icon={faHeart} className="heart" />
+            <span className="post__item__likes__counter">
+              <strong>538k</strong>
+            </span>
+            <FontAwesomeIcon icon={faComment} className="comment" />
+            <span className="post__item__comments__counter">
+              <strong>2,572</strong>
+            </span>
+          </div>
         </div>
       </div>
     </div>
