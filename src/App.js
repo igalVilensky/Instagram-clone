42ea4a4fcef62fa17f4f20c406a73998015438f9
igalv <vilenskyigal@gmail.com>
2021-06-12 14:42:45 +0200

    props and json

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import TopMenu from "./components/TopMenu";
 import MainChannelInfo from "./components/MainChannelInfo";
 import Posts from "./components/Posts";
 import PostsImages from "./components/PostsImages";
+import Counters from "./counters.json";
 
 function App() {
+  const [counter, setCounter] = useState(Counters);
+  console.log(Counters);
+
   return (
     <div className="main-wrapper">
       <TopMenu />
       <MainChannelInfo />
       <Posts />
-      <PostsImages />
+      <PostsImages counter={counter} />
     </div>
   );
 }
--- src/components/PostsImages.js
+++ src/components/PostsImages.js
@@ -1,159 +1,32 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComment, faHeart } from "@fortawesome/free-solid-svg-icons";
-function PostsImages() {
+function PostsImages(props) {
+  const { counter } = props;
+  const setCounter = counter.map((obj) => {
+    const { id, title, likes, comments } = obj;
+    return (
+      <div key={id} className="main-wrap">
+        <img
+          src="https://quietly-image-uploads.s3.amazonaws.com/image_9912_1280px_5a2c3c86cd8e4ae6a8355474364a5bdc.jpeg"
+          alt={title}
+        ></img>
+        <div className="second-div">
+          <FontAwesomeIcon icon={faHeart} className="heart" />
+          <span className="post__item__likes__counter">
+            <strong>{likes}</strong>
+          </span>
+          <FontAwesomeIcon icon={faComment} className="comment" />
+          <span className="post__item__comments__counter">
+            <strong>{comments}</strong>
+          </span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
-      <div className="posts__items__wrap">
-        <div className="main-wrap">
-          <img
-            src="https://images-na.ssl-images-amazon.com/images/I/A1yGF8BpRoL.jpg"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-        <div className="main-wrap">
-          <img
-            src="https://static-cse.canva.com/blob/130581/07-gross_IG_hubs_1_perspective.png"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-        <div className="main-wrap">
-          <img
-            src="https://media.timeout.com/images/103880032/image.jpg"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="posts__items__wrap">
-        <div className="main-wrap">
-          <img
-            src="https://blog.hubspot.com/hs-fs/hubfs/no-your-city-instagram-radio-city.png?width=278&name=no-your-city-instagram-radio-city.png"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-        <div className="main-wrap">
-          <img
-            src="https://cms.hostelworld.com/hwblog/wp-content/uploads/sites/2/2017/09/abcccccccccccccccc.jpg"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-        <div className="main-wrap">
-          <img
-            src="https://i.guim.co.uk/img/media/5c584d158992966c11c5f401e5a52e71014954c5/0_289_3992_2396/master/3992.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=7ec3e1fb9911d1f87ba3455a5877ea41"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="posts__items__wrap">
-        <div className="main-wrap">
-          <img
-            src="https://images.unsplash.com/photo-1612450632008-22c2a5437dc1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHNxdWFyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-        <div className="main-wrap">
-          <img
-            src="https://media1.popsugar-assets.com/files/thumbor/VEGiJmgc7O2_cRn0RPX87MuyE-I/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/03/14/850/n/41420188/e198c7db5c8aaaa3a0f375.84833098_/i/Best-Instagram-Spots-New-York-City.jpg"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-        <div className="main-wrap">
-          <img
-            src="http://annetravelfoodie.com/wp-content/uploads/2018/11/elbhilharmonie-hamburg-instagram-1024x1024.png"
-            alt="post"
-          ></img>
-          <div className="second-div">
-            <FontAwesomeIcon icon={faHeart} className="heart" />
-            <span className="post__item__likes__counter">
-              <strong>538k</strong>
-            </span>
-            <FontAwesomeIcon icon={faComment} className="comment" />
-            <span className="post__item__comments__counter">
-              <strong>2,572</strong>
-            </span>
-          </div>
-        </div>
-      </div>
+      <div className="posts__items__wrap">{setCounter}</div>
     </div>
   );
 }
